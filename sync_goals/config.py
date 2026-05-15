b900e999989402fa675d6ef150f4f51f121b0371
honza <honza@jMacOS.local>
2026-05-15 15:49:14 +0200

    config.py modified.

--- sync_goals/config.py
+++ sync_goals/config.py
@@ -11,24 +11,24 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 # Configuration Constants
 API_VERSION = "v9.1"
 GOALS_TO_ADD = [
-    "P53 Customer Requirements",
-    "P53 Technical Requirements",
-    "P53 Development & Technical Documentation in Dev environment",
-    "P53 Set up IAM in Dev environment",
-    "P53 Technical QA Testing in UAT environment",
-    "P53 Functional QA Testing in UAT environment",
-    "P53 Deployment from Dev to Prod environment",
-    "P53 Set up IAM in Prod environment",
-    "P53 Technical QA Testing in Prod environment",
-    "P53 Functional QA Testing in Prod Environment"
+    "P53 01 Customer Requirements",
+    "P53 02 Technical Requirements",
+    "P53 03 Development & Technical Documentation in Dev environment",
+    "P53 04 Set up IAM in Dev environment",
+    "P53 05 Technical QA Testing in UAT environment",
+    "P53 06 Functional QA Testing in UAT environment",
+    "P53 07 Deployment from Dev to Prod environment",
+    "P53 08 Set up IAM in Prod environment",
+    "P53 09 Technical QA Testing in Prod environment",
+    "P53 10 Functional QA Testing in Prod Environment"
 ]
 PROJECT_FILTER = "contains(msdyn_subject, 'IAM Security')"
 
 GOAL_MAPPING = {
-    "P53 01 Gather Requirements": "P53 Customer Requirements",
-    "P53 02 Development + Documentation": "P53 Development & Technical Documentation in Dev environment",
-    "P53 03 Quality Assurance / QA": "P53 Technical QA Testing in UAT environment",
-    "P53 04 Release Documentation for End Users": "P53 Functional QA Testing in UAT environment",
-    "P53 05 Deployment to Production": "P53 Deployment from Dev to Prod environment",
-    "P53 06 Security & IAM Implementation": "P53 Set up IAM in Prod environment"
+    "P53 01 Gather Requirements": "P53 01 Customer Requirements",
+    "P53 02 Development + Documentation": "P53 03 Development & Technical Documentation in Dev environment",
+    "P53 03 Quality Assurance / QA": "P53 05 Technical QA Testing in UAT environment",
+    "P53 04 Release Documentation for End Users": "P53 06 Functional QA Testing in UAT environment",
+    "P53 05 Deployment to Production": "P53 07 Deployment from Dev to Prod environment",
+    "P53 06 Security & IAM Implementation": "P53 08 Set up IAM in Prod environment"
 }
